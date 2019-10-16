662f8eebcacb1dbc5aa7a5688a48af83c2bf726e
nurdeafni <nurdeafni@gmail.com>
2019-10-15 20:47:14 -0700

    Belajar Picker

--- src/Persegi.js
+++ src/Persegi.js
@@ -4,20 +4,31 @@ import {
     Text,
     StyleSheet,
     Button,
-    TextInput
+    TextInput,
+    Picker
 } from 'react-native';
 
 class Persegi extends React.Component{
     //#1 inisialisasi state
-    state= {panjang:0, lebar:null, luas:null};
+    state= {panjang:0, lebar:null, formula:null, rumus: null, keliling: null};
     //#2 methode untuk merubah state luas
     hitungLuas= () =>{
         let panjang= this.state.panjang; //tampung nilai state akhir pada variable panjang
         let lebar= this.state.lebar; //tampung nilai state akhir pada variable panjang
-        let luas = parseInt(panjang) * parseInt(lebar); //menghitung luas persegi dan lebar harus dikonversikan 
-        //ke int karena asalnya string
-        this.setState({luas : luas}) // set state luas
         
+        if(this.state.rumus=="luas"){
+            let luas = parseInt(panjang) * parseInt(lebar) 
+            this.setState({formula : luas})
+            
+        }else if(this.state.rumus== 'keliling'){
+            let keliling = (parseInt(panjang) + parseInt(lebar)) * 2
+            this.setState({formula : keliling})
+
+            
+        }
+
+        
+        console.log(this.state)
     }
     render(){
         // #3 read  component state
@@ -40,7 +51,16 @@ class Persegi extends React.Component{
                         {lebar: InputLebar})}
                     value={this.state.lebar}
                 />
+                <Picker
+                selectedValue={this.state.rumus}
+                style={{heigh:100, width: 300}}
+                onValueChange={(itemValue, itemIndex)=>
+                    this.setState({rumus: itemValue})}>
 
+                <Picker.Item label="Pilihan Tipe" value=""/>
+                <Picker.Item label="Luas" value="luas"/>
+                <Picker.Item label= "Keliling" value="keliling"/>
+                </Picker>
                 <Button
                     title="Hitung Luas"
                     onPress={() => {
@@ -48,7 +68,7 @@ class Persegi extends React.Component{
                 }}
                 />
 
-                <Text>{this.state.luas}</Text> 
+                <Text>{this.state.formula}</Text> 
 
             </View>
         );
